1c1053e6c0950c5df1d9d24072eca9e1c59a02a9
jyx <email@example.com>
2019-09-07 12:45:50 +0800

    二叉树的节点

---
+++ src/binaryTree/TreeNode.java
@@ -0,0 +1,17 @@
+package binaryTree;
+
+/**
+ * 二叉树的节点
+ * @author Jiang.YX
+ * @version 1.0
+ * @date 2019/9/7 12:44
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
