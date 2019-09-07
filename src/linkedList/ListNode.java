709e3f86b809fbe2bf8417d8293200db16a0a5c3
jyx <email@example.com>
2019-09-07 12:19:05 +0800

    单向链表的节点

---
+++ src/linkedList/ListNode.java
@@ -0,0 +1,15 @@
+package linkedList;
+
+/**
+ * 单向链表的节点
+ * @author Jiang.YX
+ * @date 2019/9/7
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x){
+        val=x;
+        next=null;
+    }
+}
